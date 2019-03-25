b3733977593d5e4cd2a5e8724265ddf70bbb5a1e
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2019-03-25 13:30:45 -0400

    add files, animation on complete

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@
 
 console.log("javascript linked up");
 
+	const 	badge		= document.querySelector('img'),
+			headerText	= document.querySelector('h1');
+			footerText	= document.querySelector('h3');
+
+	function animImage() {
+		TweenMax.to(badge, 1.2, { scaleX: 1, scaleY: 1, rotation: 50, ease:Elastic.easeOut, onComplete:animFooterText});
+	}		
+
+	function animFooterText() {
+		TweenMax.to(footerText, 0.7, { scaleX: 2, scaleY: 2, ease:Bounce.easeOut})
+	}
+
+	badge.addEventListener('mouseover', animImage);
+
 })();
